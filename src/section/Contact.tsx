0b4490157661c65c4f790e6523f54a01d326887f
ImAdityaa12 <117254771+ImAdityaa12@users.noreply.github.com>
2023-09-16 15:00:46 +0530

    Done

--- src/section/Contact.tsx
+++ src/section/Contact.tsx
@@ -11,7 +11,7 @@ function Contact() {
       <h2 className="contact-title">What&apos;s Next?</h2>
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
-        Although I&apos;m not currently looking for any new opportunities, my inbox
+        Although I am currently looking for an new opportunities, my inbox
         is always open. Whether you have a question or just want to say hi, I&apos;ll
         try my best to get back to you!
       </p>
